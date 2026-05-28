44e4d2b5a8c3c958602493a7f32b37acc3a816df
lowqualityrkomi <casoni.mirko@gmail.com>
2026-05-28 08:55:13 +0200

    fix icon in the windows app bar

--- src/main/index.ts
+++ src/main/index.ts
@@ -12,7 +12,7 @@ function createWindow(): void {
     minHeight: 650,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon: process.platform === 'linux' ? icon : join(__dirname, '../../resources/icon.ico'),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
